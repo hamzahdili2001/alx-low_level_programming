fc40850267df83ef29da7077fdb137ea81c716d1
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-16 17:41:04 +0100

    oh yeah, it's C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print string to the screen
+*
+* return : it's return 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
