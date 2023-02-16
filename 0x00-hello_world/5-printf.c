8465327b47dbb7dbe95c29d53bc6757357fd7286
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-16 17:59:50 +0100

    oh yeah, it's C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starts the program
+ * Description: printf prints the string inside of it with new line
+ * Return: it's return 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
