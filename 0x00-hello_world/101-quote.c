78624ba3fb27a7c83178593cb75ee56665708830
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-16 18:53:23 +0100

    that was a burnout in my brain

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,13 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - the main function in the program
- * Description: print the string without puts
- * Return: returns 1
+ * main - Starting the program
+ *
+ * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-long l = 59;
-long fd = 1;
-long syscall = 1;
-long ret = 0;
-__asm__ ("syscall"
-: "=a" (ret)
-: "a" (syscall),
-"D" (fd),
-"S" (s),
-"d" (l));
-return (1);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
+	return (1);
 }
