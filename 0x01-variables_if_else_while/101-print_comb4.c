c93139a6f38b506e9d2def5842568c83db9560ea
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-17 20:12:50 +0100

    combo4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - runs the program
+ * Desription: form 012 - 789 with commas
+ * Return: 0
+ */
+int main(void)
+{
+	int i, e, g;
+
+	i = 48;
+	e = 48;
+	g = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			g = 48;
+			while (g < 58)
+			{
+				if (e != i && e != g && i != g && e < i && i < g)
+				{
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 56 && e == 55 && g == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				g++;
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar(10);
+	return (0);
+}
