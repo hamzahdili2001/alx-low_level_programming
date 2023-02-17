a4cd7003b97faab2bdae5befdb9834d45efde877
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-17 19:10:02 +0100

    betty please

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - runs the program
+ * Description: writing alphabet exept q and e
+* Return: 0
+*/
+
+int main(void)
+{
+	int a; /* 113 and 101*/
+	for (a = 97; a <= 122; a++)
+	{
+		if (a == 113 || a == 101)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(a);
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
