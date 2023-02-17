752117fdc1e40eea31b5ef36eb2fae82c7282f94
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-17 17:17:01 +0100

    control flow : if..else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,14 +17,14 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	if (n == 0)
-		printf("%d is zero\n", n);
-
-	else if (n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
 
+	else if (n < 0)
+		printf("%d is negative\n", n);
+
 	else
-		printf("% is negative\n", n);
+		printf("% is zero\n", n);
 
 	return (0);
 }
