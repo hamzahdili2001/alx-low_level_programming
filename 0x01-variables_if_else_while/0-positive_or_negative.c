2528853192fdc021d5bd292e0ad6fcf8f33db22b
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-17 17:14:07 +0100

    control flow : if..else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - runs the program
+ * Description: This application generate a random number and check if the num\
+ber is zero, negative or positive
+ * Return: 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+	if (n == 0)
+		printf("%d is zero\n", n);
+
+	else if (n > 0)
+		printf("%d is positive\n", n);
+
+	else
+		printf("% is negative\n", n);
+
+	return (0);
+}
