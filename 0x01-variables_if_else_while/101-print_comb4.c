281aef892ec6061603fee0e903b99de7d5a348e5
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-17 22:09:19 +0100

    a good alogorithm than before

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - runs the program
+ * Desription: form 012 - 789 with commas
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 1000; i++)
+	{
+		int d1, d2, d3;
+
+		d1 = i / 100;
+		d2 = (i / 10) % 10;
+		d3 = i % 10;
+
+		if (i != 12 && 1 != 102 && i != 120 && i != 101
+			 && i != 210)
+		{
+			if (i != 123)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar(d1 + '0');
+		putchar(d2 + '0');
+		putchar(d3 + '0');
+	}
+
+	putchar(10);
+	return (0);
+}
