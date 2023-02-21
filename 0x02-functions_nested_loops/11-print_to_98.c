2476960d95dc02042280fde5905a57da437192e8
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-21 21:53:52 +0100

    print number from n number to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 #include "main.h"
+/**
+ * print_to_98 - Function
+ * Description: print all natural number from n to 98
+ * @n: start number
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	int start;
+
+	for (start = n; start <= 98; start++)
+	{
+		printf("%d, ", start);
+	}
+	putchar(10);
+}
+
