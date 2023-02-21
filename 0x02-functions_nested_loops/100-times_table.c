0d02f3f91831c55d0f59b2baa00ad1468f18c4f6
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-21 23:24:58 +0100

    print times table with conditions

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - Function.
+ * Description: prints times table form n number with conditions
+ * @n: the giving value
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int result;
+
+				result = i * j;
+
+				if (j == 0)
+				{
+					printf("%d", result);
+				}
+				else
+				{
+					printf(",%3d", result);
+				}
+			}
+			printf("\n");
+		}
+	}
+}
