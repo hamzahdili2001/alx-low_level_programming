143200caf5fa97e362c4d47a2da530c0616397b3
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-21 17:27:43 +0100

    print alphabet from a - z

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,25 @@
 #include "main.h"
+
+/**
+ * main - starts the program
+ * Description: print alphabet from a - z
+ * Return: 0
+*/
+
+int main(void)
+{
+
+	/* 97 to 122 */
+
+	int count;
+
+	for (count = 97; count < 123; count++)
+	{
+		_putchar(count);
+	}
+
+	_putchar(10);
+
+	return (0);
+}
+
