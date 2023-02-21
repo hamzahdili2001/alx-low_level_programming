4437be7ec451476d48d637d70049c30ceac89016
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-21 17:15:24 +0100

    print '_putchar'

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * main - start the program
+ * Description: print _putchar
+ * Return: 0
+*/
+
+int main (void)
+{
+	int length, i;
+
+	char chars[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	length = sizeof(chars) / sizeof(char);
+
+	for (i = 0; i < length; i++)
+	{
+		_putchar(chars[i] + 0);
+	}
+
+	_putchar(10);
+
+	return (0);
+}
+
