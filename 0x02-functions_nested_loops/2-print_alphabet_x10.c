afca69a558222c7d11c97e2b08efd87eebc59a05
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-21 18:16:17 +0100

    print alphabet 10 times from a - z

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * print_alphabet_x10 - Function
+ * Description: Prints 10 lines of alphabet from a to z
+ * Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 11, i++)
+	{
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
+		_putchar(10);
+	}
+}
+
