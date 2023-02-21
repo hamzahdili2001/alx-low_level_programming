eeab59c034da376a0b941a7cc623e7001b745411
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-21 18:49:02 +0100

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,21 @@
 #include "main.h"
+
+/**
+ * _isalph - function
+ * Description : check the value if it alphabet
+ * @c : takes integer value
+ * Return: 0 if false or 1 if true
+ */
+
+int _isalph(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
