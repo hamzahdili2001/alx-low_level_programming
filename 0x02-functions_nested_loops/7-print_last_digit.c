4f0d3fcae4e9e503ff1c593e0472bbe68f1ae3e7
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-21 20:16:13 +0100

    last number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * print_last_digit - Function
+ * Description: get the last value of number
+ * @d: the giving value
+ * Return: the value.
+ */
+int print_last_digit(int d)
+{
+	int last_number;
+
+	last_number = d % 10;
+
+	if (last_number < 0)
+	{
+		_putchar(-last_number + 48);
+		return (-last_number);
+	}
+	else
+	{
+		_putchar(last_number + 48);
+		return (last_number);
+	}
+}
+
