cd0d9b6535fa39ae768d9e9ff300e72d126bcb33
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-22 00:31:43 +0100

    First 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Function
+ * Description: Fibonacci sequence up to 4,000,000
+ * Return: 0.
+ */
+
+int main(void)
+{
+	long int prev, curr, sum;
+
+	prev = 1;
+	curr = 2;
+	sum = 0;
+
+	while (curr <= 4000000)
+	{
+		if (curr % 2 == 0)
+		{
+			sum += curr;
+		}
+		int next;
+
+		next = prev + curr;
+
+		prev = curr;
+
+		curr = next;
+	}
+	printf("%ld\n", sum);
+	return 0;
+}
