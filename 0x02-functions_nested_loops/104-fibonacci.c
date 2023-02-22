3fef8228d544611019adfca3cdb34a36df05e75a
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-22 01:08:31 +0100

    check me with A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,31 +5,42 @@
  * Description: print the first 98 Fibonacci numbers
  * Return: 0.
  */
+
 int main(void)
 {
-	long int fib1, fib2, fib3, fib4;
-	int i;
+	int c;
+	long int previousFibonacci = 1, currentFibonacci = 2, nextFibonacci;
 
-	fib1 = 1;
-	fib2 = 2;
+	printf("%ld, %ld", previousFibonacci, currentFibonacci);
 
-	printf("%ld, %ld, ", fib1, fib2);
-
-
-	for (i = 3; i <= 98; i += 2)
+	for (c = 0; c < 96; c++)
 	{
+		if (currentFibonacci < 0)
+		{
+			break;
+		}
+		else if (currentFibonacci < 1000000000)
+		{
+			nextFibonacci = previousFibonacci + currentFibonacci;
+			printf(", %ld", nextFibonacci);
+			previousFibonacci = currentFibonacci;
+			currentFibonacci = nextFibonacci;
+		}
 
-		fib3 = fib1 + fib2;
-		fib4 = fib2 + fib3;
+		else
+		{
 
-		printf("%ld, %ld, ", fib3, fib4);
+			long int carry = (previousFibonacci + currentFibonacci) / 1000000000;
+			long int nextLowerPart = (previousFibonacci + currentFibonacci)
+			% 1000000000;
 
-		fib1 = fib3;
-		fib2 = fib4;
+			nextFibonacci = currentFibonacci + carry;
+			printf(", %ld%09ld", nextFibonacci, nextLowerPart);
+			previousFibonacci = currentFibonacci;
+			currentFibonacci = nextFibonacci;
+		}
 	}
-	fib3 = fib1 + fib2;
-
-	printf("%ld\n", fib3);
-
+	printf("\n");
 	return (0);
 }
+
