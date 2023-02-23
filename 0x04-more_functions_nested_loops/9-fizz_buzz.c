7b4cdf00381ec9b8e4dcf6ab0cc12885198ca2e9
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-23 18:55:21 +0100

    some old codewars challenges

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz Algorithm
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+
+	}
+	putchar(10);
+	return (0);
+}
+
