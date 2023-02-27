b6795308aa2cd3c3c64968bc301862b0c984839e
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-27 22:04:50 +0100

    Fixed: wrong output in the checker

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -36,9 +36,19 @@ void puts_half(char *str)
 
 	length = get_length(str) + 1;
 
-	for (i = length / 2; i < length; i++)
+	if (length % 2 == 0)
 	{
-		_putchar(*(str + i));
+		for (i = length / 2; i <= length; i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	else
+	{
+		for (i = (length - 1) / 2; i <= length; i++)
+		{
+			_putchar(*(str + i));
+		}
 	}
 	_putchar(10);
 }
