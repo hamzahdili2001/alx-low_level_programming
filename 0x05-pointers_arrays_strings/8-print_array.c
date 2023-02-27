e70a0cde614d0423885d6dafcd28cab56f114aa1
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-27 20:11:57 +0100

    changing the whole code for one space error

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,20 +10,13 @@ void print_array(int *a, int n)
 
 	int i;
 
-	if (n < 0)
+	for (n--; n >= 0; n--, i++)
 	{
-		return;
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
-		{
-			if (i == n - 1)
-				printf("%d\n", *(a + i));
-			else
-				printf("%d, ", *(a + i));
-		}
+		printf("%d", *(a + i));
 
+		if (n > 0)
+			printf(", ");
 	}
+	printf("\n");
 }
 
