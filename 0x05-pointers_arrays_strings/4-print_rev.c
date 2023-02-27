a1eadaa3caa5352a65730ddd4e17a41a71a9eed7
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-27 18:30:53 +0100

    reverse function

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+ * get_length - Gets the length of String
+ * @str: the string
+ * Return: length of str
+*/
+
+int get_length(char *str)
+{
+	unsigned int length;
+
+	length = 0;
+
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * print_rev - print string in reverse
+ * @s: string
+ * Return:
+ */
+
+void print_rev(char *s)
+{
+	int count;
+
+	count = get_length(s);
+
+	while (count != -1)
+	{
+		_putchar(*(s + count));
+
+		count--;
+	}
+	_putchar(10);
+}
+
