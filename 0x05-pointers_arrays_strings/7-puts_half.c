b1231c9e60a7a6ea4859ab33210db4bc21a4491a
hamzahdili2001 <hdilihamza955@gmail.com>
2023-02-27 21:39:48 +0100

    Adding puts_half function

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+
+/**
+ * get_length - Gets the length of String
+ * @str: the string
+ * Return: length of str
+*/
+
+int get_length(char *str)
+{
+	unsigned int length;
+
+	length = 0;
+
+
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+
+
+/**
+ * puts_half - prints half of a string
+ * @str: string given
+ *
+ * Return:
+ */
+
+void puts_half(char *str)
+{
+	int length, i;
+
+	length = get_length(str) / 2;
+
+	for (i = length; i < get_length(str); i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar(10);
+}
