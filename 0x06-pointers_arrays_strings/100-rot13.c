2c947a96c1e1e702243a3defa3859dddcb27bf5b
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-01 22:26:48 +0100

    task 100 done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+
+/**
+ * rot13 - rotate stirng by 13 places
+ * @str: string given
+ * Return: rot13
+ */
+
+char *rot13(char *str)
+{
+	int c = 0, i;
+
+	char alph[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(str + c) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if(*(str + c) == alph[i])
+			{
+				*(str + c) = rot[i];
+				break;
+			}
+		}
+		c++;
+	}
+
+	return (str);
+}
+
