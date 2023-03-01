23858d5b1e52f28d91b8cddb87f42dca167f0e40
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-01 17:06:20 +0100

    task of concat with n bytes done

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,48 @@
+
+/**
+ * _strlen - gets the length of string
+ * @s: the string
+ * Return: length
+*/
+
+int _strlen(char *s)
+{
+	unsigned int length;
+
+	length = 0;
+
+	while (*(s + length) != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
+
+/**
+ * _strncat - function that concat two strings using n bytes form src
+ * @n: the bytes
+ * @dest:the string one
+ * @src: the string two
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_length, i;
+
+	i = 0;
+	while (i < n && *(src + i) != '\0')
+	{
+		dest_length = _strlen(dest);
+
+		*(dest + dest_length) += *(src + i);
+
+		i++;
+	}
+
+	*(dest + dest_length) += '\0';
+
+	return (dest);
+}
+
