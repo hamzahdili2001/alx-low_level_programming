3bf018fab2589dae0229b59f87691345a72f2f58
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-01 17:44:38 +0100

    task corrected

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -29,19 +29,17 @@ int _strlen(char *s)
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_length, i;
+	int dest_length = _strlen(dest), i = 0;
 
-	i = 0;
 	while (i < n && *(src + i) != '\0')
 	{
-		dest_length = _strlen(dest);
-
-		*(dest + dest_length) += *(src + i);
+		*(dest + dest_length) = *(src + i);
 
 		i++;
+		dest_length++;
 	}
 
-	*(dest + dest_length + 1) += '\0';
+	*(dest + dest_length) = '\0';
 
 	return (dest);
 }
