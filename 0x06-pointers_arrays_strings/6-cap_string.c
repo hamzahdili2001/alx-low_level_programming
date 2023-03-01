0e7c3bd26bd8b4e4deb971ab382950353273a29b
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-01 20:38:58 +0100

    task-6 done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,30 @@
 /**
  * cap_string - capitalize
- * @str: string given.
+ * @s: string given.
  * Return: pointer/string capitalized.
  */
 
-
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i, cap_next = 1, len = 0;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (str[len] != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		len++;
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
+		count++;
 	}
-
-	for (i = 0; i < len; i++)
-	{
-		if (cap_next && str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - 'a' + 'A';
-			cap_next = 0;
-		}
-		else if (!cap_next && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
-					|| str[i] == ',' || str[i] == ';' || str[i] == '.'
-					|| str[i] == '!' || str[i] == '?' || str[i] == '"'
-					|| str[i] == '(' || str[i] == ')' || str[i] == '{'
-					|| str[i] == '}'))
-		{
-			cap_next = 1;
-		}
-		else
-		{
-			cap_next = 0;
-		}
-	}
-
-	return (str);
+	return (s);
 }
+
