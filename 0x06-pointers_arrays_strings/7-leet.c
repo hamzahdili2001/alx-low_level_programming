7eb07d5f76ed22678e8f3318418d23adb83f6bbf
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-01 21:55:09 +0100

    task-7 done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * leet - encode a string into leet => 1337
+ * @str: - string that will be encoded
+ * Return: encoded String
+*/
+
+char *leet(char *str)
+{
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (*(str + count) != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*(str + count) == low_letters[i] || *(str + count) == upp_letters[i])
+			{
+				*(str + count) = numbers[i];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (str);
+}
