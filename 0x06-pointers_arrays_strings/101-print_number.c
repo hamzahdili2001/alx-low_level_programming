0329875b9ef7475ecc225d3e9ebad59435982a6d
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-01 22:40:48 +0100

    task 101 done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: an integer given to it
+ * Return: integer
+ * */
+
+void print_number(int n)
+{
+	unsigned int mod, div, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+	mod = n * -1;
+	}
+	else
+	{
+		mod = n;
+	}
+
+	div = mod;
+	count = 1;
+
+	while (div > 9)
+	{
+		div /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((mod / count) % 10) + 48);
+	}
+}
+
