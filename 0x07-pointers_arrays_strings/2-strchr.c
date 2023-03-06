e0a181336890697b3f229d3639ac3e8def4fc5a2
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-06 10:36:24 +0100

    task 2 correction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,11 +11,10 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	while ((*s + i) != '\0')
+	for (; (*s + i) != '\0'; i++)
 	{
 		if (*(s + i) == c)
 			return (s + i);
-		i++;
 	}
 	if (*(s + i) == c)
 		return (s + i);
