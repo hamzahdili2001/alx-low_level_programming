1ea3624e38302bc1388c7387a950cf4fb40244a0
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-08 17:15:33 +0100

    fixed: no loop

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,23 +16,37 @@ int _strlen(char *s)
 	return (i);
 }
 /**
+ * is_palindrome_helper - recursive helper function for is_palindrome
+ * @s: the string to check
+ * @start: the starting index of the current substring being checked
+ * @end: the ending index of the current substring being checked
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
+int is_palindrome_helper(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+
+	if (s[start] != s[end])
+		return (0);
+
+	return (is_palindrome_helper(s, start + 1, end - 1));
+}
+
+/**
  * is_palindrome - checks if a string is a palindrome
  * @s: the string to check
+ *
  * Return: 1 if the string is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-	int i, len = _strlen(s);
+	int len = _strlen(s);
 
 	if (len <= 1)
 		return (1);
 
-	for (i = 0; i < len / 2; i++)
-	{
-		if (s[i] != s[len - i - 1])
-			return (0);
-	}
-
-	return (1);
+	return (is_palindrome_helper(s, 0, len - 1));
 }
 
