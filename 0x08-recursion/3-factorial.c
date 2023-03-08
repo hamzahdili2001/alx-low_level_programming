ea9e78b285151a20366e4fd4ae146841c99d3e94
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-08 14:31:13 +0100

    task 3 done

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+
+/**
+ * factorial - calculate the factorial of n
+ * @n: Number
+ * Return: factorial
+*/
+
+int factorial(int n)
+{
+
+	if (n < 0)
+		return (-1);
+
+	else if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
+
