4cd6e8bba505697131ae07bd64405943671a576e
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-10 18:53:20 +0100

    task 1 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of arguments passed to file
+ * @argc: numbers of values
+ * @argv: array of arguments
+ * Return: 0
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
