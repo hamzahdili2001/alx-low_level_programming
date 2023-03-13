a0f428e8f3bc80d8e8dc6d92d56a7ec616c7b168
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-13 17:30:45 +0100

    task 2 done

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+
+/**
+ * sizeOfStr - get Length of string
+ * @string: given string
+ * Return: length of stirng
+*/
+size_t sizeOfStr(char *string)
+{
+	int size = 0;
+
+	while (*(string + size))
+	{
+		size++;
+	}
+
+	return (size);
+}
+/**
+ * str_concat - Concat two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: a newly allocated space in memory which contains the
+ * contents of s1, followed by the contents of s2, and null terminated
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	size_t size = sizeOfStr(s1) + sizeOfStr(s2);
+	char *newStr;
+	unsigned int i, sizeOfNewStr, j;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	newStr = malloc(size * sizeof(char) + 1);
+
+	for (i = 0; i < sizeOfStr(s1); i++)
+	{
+		newStr[i] = s1[i];
+	}
+	sizeOfNewStr = sizeOfStr(newStr);
+
+	j = 0;
+	for (i = sizeOfNewStr; i < size; i++)
+	{
+		newStr[i] = s2[j];
+		j++;
+	}
+	newStr[size + 1] = '\0';
+	return (newStr);
+}
+
