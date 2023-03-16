b5bbde7f4df467777ad47f6e005562c42b7c73df
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-16 15:41:06 +0100

    Add files

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -16,7 +16,8 @@ int main(void)
     printf("%s", s2);
     ptr = _strcat(s1, s2);
     printf("%s", s1);
-    printf("%s", s2);
-    printf("%s", ptr);
-    return (0);
+	printf("%s", s2);
+	printf("%s", ptr);
+	return (0);
 }
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+# More Memory Allocation!
