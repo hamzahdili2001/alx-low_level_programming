3bb934e5c2f1bcd47db6c9882087b8582d5ed2a0
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-20 19:22:14 +0000

    task 1 done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - struct of a dog
+* @name: name of the dog
+* @age: age if the dog
+* @owner: the owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
