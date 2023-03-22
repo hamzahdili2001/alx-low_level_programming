11a0e2621d72973d1aa89039bbbf01fe56b5cbec
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-22 15:04:00 +0000

    Finish task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+
+/**
+ * print_name - function that callback another function
+ * Description: the function takes a pointer to the
+ * name and a pointer to function
+ * @name: name as pointer to string
+ * @f: a function that do something to the name
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
