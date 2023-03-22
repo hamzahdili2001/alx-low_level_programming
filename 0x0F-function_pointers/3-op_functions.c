d3a8d134e80c16f4753dc45108fe38b9d72f057c
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-22 18:24:25 +0000

    add task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add to number
+ * @a: number 1
+ * @b: number 2
+ * Return: sum of two number
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - sub two number
+ * @a: number 1
+ * @b: number 2
+ * Return: sub of two number
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: multiply of two number
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two number
+ * @a: number 1
+ * @b: number 2
+ * Return: divition of two numbers
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulize two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: modulo of two numbers
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
