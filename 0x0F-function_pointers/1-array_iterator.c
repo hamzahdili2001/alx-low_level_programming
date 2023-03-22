23797100bcbaa75fa7b01caf1b7bb5c2a8ff48f6
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-22 15:56:38 +0000

    add task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,8 +11,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (array && action)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+			action(array[i]);
 	}
 }
