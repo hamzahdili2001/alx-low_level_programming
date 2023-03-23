cc53bf3b8b5b45b6e8ca1ed3f8da088a53a81978
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-23 20:39:28 +0000

    solved:1: print all number passed to a function by seperator

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -1,5 +1,5 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
 /**
  * main - check the code
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_numbers(", ", 4, 0, 98, -1024, 402);
+    print_numbers(NULL, 4, 0, 98, -1024, 402);
     return (0);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,20 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
-	if (separator != NULL)
+	va_start(args, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(args, n);
-		for (i = 0; i < n; i++)
+		if (i != n - 1)
 		{
-			if (i != n - 1)
-				printf("%d%s", va_arg(args, int), separator);
+			printf("%d", va_arg(args, int));
+			if (separator == NULL)
+				printf(" ");
 			else
-				printf("%d\n", va_arg(args, int));
+				printf("%s", separator);
 		}
-	}
+		else
+			printf("%d\n", va_arg(args, int));
 
+	}
 	va_end(args);
 }
