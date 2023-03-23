4d66176ccb8b7d9aada2de5f335038dad35d9951
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-23 21:17:19 +0000

    fixed spaces

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 
-		if (i != n -1 && separator != NULL)
+		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 
 	}
