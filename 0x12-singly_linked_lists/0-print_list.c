9232dace279c42a03599d29bffe854fa81de5d1f
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-30 21:20:02 +0000

    Add: else to the condition

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,10 +14,9 @@ size_t print_list(const list_t *h)
 	while (current != NULL)
 	{
 		if (current->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
-		printf("[%d], %s\n", current->len, current->str);
+		else
+			printf("[%d], %s\n", current->len, current->str);
 		current = current->next;
 		count++;
 	}
