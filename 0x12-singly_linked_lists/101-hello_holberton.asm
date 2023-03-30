d01ad709e6e2e7b35a3d17269fc3364239a744c3
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-30 23:02:55 +0000

    Fix: syntax

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,19 @@
 section .data
-    hello db "Hello, Holberton", 0Ah, 0
-    format db "%s"
+hello:	db "Hello, Holberton",10,0
+format:	db "%s",0
 
-section .text
-    global main
-    extern printf
+   section .text
+   global main
+   extern printf
 
 main:
-    push format
-    push hello
-    call printf
-    add rsp, 16
-    ret
+   push rbp
+   mov rbp, rsp
+
+   mov rsi, hello
+   mov rdi, format
+   call printf
+
+   pop rbp
+   mov rax, 0
+   ret
