12a809c383c75a02f399149681d6432f47b88623
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-30 21:26:29 +0000

    removed: current variable

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,16 @@
 */
 size_t print_list(const list_t *h)
 {
-	const list_t *current = h;
 	size_t count = 0;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u], %s\n", current->len, current->str);
-		
-		current = current->next;
+			printf("[%u], %s\n", h->len, h->str);
+
+		h = h->next;
 		count++;
 	}
 
