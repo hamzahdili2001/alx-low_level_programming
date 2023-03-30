ba0742e5c5d57a54f9388c529da1bd609cb253dc
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-30 21:11:01 +0000

    Solved: Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_list - function that prints element of a linkedList
+ * @h: the head of the list
+ * Return: count of the element in the list
+*/
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	int count = 0;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		printf("[%d], %s\n", current->len, current->str);
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
+
