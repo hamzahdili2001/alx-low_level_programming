b59c03f7aae6ea3bc22f9711ef948f1084066ab0
hamzahdili2001 <hdilihamza955@gmail.com>
2023-03-30 21:22:31 +0000

    change: %d to %u

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,8 @@ size_t print_list(const list_t *h)
 		if (current->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d], %s\n", current->len, current->str);
+			printf("[%u], %s\n", current->len, current->str);
+		
 		current = current->next;
 		count++;
 	}
