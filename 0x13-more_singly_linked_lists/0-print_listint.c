980ca9e496c17601b463b553b9b352fba1c04c1c
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-03 20:18:34 +0000

    Done: Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,6 +13,11 @@ size_t print_listint(const listint_t *h)
 	const listint_t *current = h;
 	int count;
 
+	if (current == NULL)
+	{
+		return (-1);
+	}
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
