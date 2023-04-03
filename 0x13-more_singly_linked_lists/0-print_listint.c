6212d3a00503e1f5582d5dd74e653fd1509a49c5
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-03 20:16:55 +0000

    Done: Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_listint - function that print all elements in the list
+ * @h: head of the list
+ * Return: Length of the list
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current = h;
+	int count;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+
+	return (count);
+}
