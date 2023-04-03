9895fd8d06ee8d18b610907e2dc0929306ac0322
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-03 20:22:23 +0000

    Fix: count variable was not initialized

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,12 +11,7 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *current = h;
-	int count;
-
-	if (current == NULL)
-	{
-		return (-1);
-	}
+	int count = 0;
 
 	while (current != NULL)
 	{
