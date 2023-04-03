814d926ebfa0e1948e9331bbc2195fc567e57e35
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-03 21:15:44 +0000

    Done: Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list and set the head to NULL
+ * @head: head of the list
+ * Return: Nothing
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while ((*head) != NULL)
+	{
+		current = (*head);
+		(*head) = (*head)->next;
+		free(current);
+	}
+	head = NULL;
+}
+
+
