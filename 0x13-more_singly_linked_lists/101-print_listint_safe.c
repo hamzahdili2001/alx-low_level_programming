473c5695ac58355c0bf56444fcedb64f4eede2bc
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-04 22:29:36 +0000

    Done: Task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,49 +1,85 @@
 #include "lists.h"
 #include <stdio.h>
 
+size_t print_list(const listint_t *head, const listint_t *loop_node);
+const listint_t *find_loop(const listint_t *head);
 /**
- * print_listint_safe -  function that prints a listint_t linked list.
- * @head: head of the list
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to the head of the list
  * Return: the number of nodes in the list
 */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tortoise = head, *hare = head;
-	size_t count = 0;
+	const listint_t *loop_node = find_loop(head);
+	size_t count = print_list(head, loop_node);
+
+	return (count);
+}
+
+/**
+ * find_loop - finds the node at the start of a loop in a linked list
+ * @head: pointer to the head of the list
+ *
+ * Return: a pointer to the node at the start of the loop,
+ * or NULL if there is no loop
+ */
+const listint_t *find_loop(const listint_t *head)
+{
+	const listint_t *slow, *fast;
 
 	if (head == NULL)
-		return (0);
-
-	while (tortoise != NULL && hare != NULL && hare->next != NULL)
+		exit(98);
+	slow = head;
+	fast = head;
+	while (fast != NULL && fast->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-		tortoise = tortoise->next;
-		hare = hare->next->next;
-
-		if (tortoise == hare)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 		{
-			printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-			count++;
-			tortoise = tortoise->next;
-			while (tortoise != hare)
+			slow = head;
+			while (slow != fast)
 			{
-				printf("[%p] %d\n", (void *)tortoise, tortoise->n);
-				count++;
-				tortoise = tortoise->next;
+				slow = slow->next;
+				fast = fast->next;
 			}
-			hare = head;
-			while (tortoise != hare)
-			{
-				printf("[%p] %d\n", (void *)hare, hare->n);
-				count++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
-			count++;
-			break;
+			return (slow);
 		}
+	}
+	return (NULL);
+}
+
+/**
+ * print_list - prints the contents of a linked list
+ * @head: pointer to the head of the list
+ * @loop_node: pointer to the node at the start of the loop,
+ * or NULL if there is no loop
+ *
+ * Return: the number of nodes in the list
+ */
+size_t print_list(const listint_t *head, const listint_t *loop_node)
+{
+	const listint_t *node = head;
+	size_t count = 0;
+
+	while (node != NULL)
+	{
+		printf("[%p] %d\n", (void *)node, node->n);
+		node = node->next;
 		count++;
+		if (node == loop_node)
+			break;
+	}
+	if (loop_node != NULL)
+	{
+		const listint_t *loop_start = loop_node;
+
+		do {
+			printf("[%p] %d\n", (void *)node, node->n);
+			node = node->next;
+			count++;
+		} while (node != loop_start);
+		printf("-> [%p] %d\n", (void *)loop_start, loop_start->n);
 	}
 	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,5 +30,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-size_t loop_list(const listint_t *head, const listint_t *end);
 #endif
