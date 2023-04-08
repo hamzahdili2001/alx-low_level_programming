ba32241da35be588dfa68c38e2af8b8826d5c094
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-08 01:21:37 +0000

    Done: Task 7

---
+++ 0x14-bit_manipulation/wirtePass.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    unsigned int var1 = 0x46c6f48;
+    unsigned char var2 = 0xff;
+    unsigned int var3 = 0;
+    
+    char password[5]; // password buffer
+    
+    // generate password based on var1, var2, and var3
+    for (int i = 0; i < 4; i++) {
+        password[i] = ((var1 >> (8 * i)) & 0xff) ^ ((var3 >> (8 * i)) & 0xff) ^ var2;
+    }
+    password[4] = '\0'; // terminate the string
+    
+    // print the final ASCII value of var1 to a file
+    FILE *file = fopen("output.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        fprintf(file, "%c", (var1 >> (8 * i)) & 0xff);
+    }
+    fclose(file);
+    
+    return 0;
+}
+
