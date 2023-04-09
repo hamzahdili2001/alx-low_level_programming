3dfb87a349fdcbf09c1d2bc761b709d8d5fcff73
hamzahdili2001 <hdilihamza955@gmail.com>
2023-04-09 01:03:25 +0000

    Change: using the old concept

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,5 @@
-#include "main.h"
-#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 /**
  * append_text_to_file - appending text into a file
  * @filename: the file
@@ -8,16 +8,29 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	FILE *file;
+	int file;
+	ssize_t wsize, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = fopen(filename, "a");
-
 	if (text_content != NULL)
-		fputs(text_content, file);
+	{
+		while (text_content[len])
+			len++;
+	}
 
-	fclose(file);
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	wsize = write(file, text_content, len);
+	if (wsize == -1)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
 	return (1);
 }
