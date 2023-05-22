5d67029ae8c386ae237c02e3d02ec3d03b505487
hamzahdili2001 <hdilihamza955@gmail.com>
2023-05-22 13:11:36 +0100

    task 2: dynamic library -> done

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,54 @@
+/**
+ * add - add two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subs two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod two numbers
+ * @a: number one
+ * @b: number two
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
