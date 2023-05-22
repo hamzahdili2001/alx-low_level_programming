0185058db74aac68a55389a275e6c90457ebdd65
hamzahdili2001 <hdilihamza955@gmail.com>
2023-05-22 14:44:10 +0100

    add: file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-export LD_PRELOAD=./win.so
+wget -P /tmp/ https://github.com/hamzahdili2001/alx-low_level_programming/raw/main/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
